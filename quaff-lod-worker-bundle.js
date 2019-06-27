3dc3120b9355d614b801744e37c6406154a78461
Shawn Murphy <smurp@smurp.com>
2019-06-27 09:20:49 +0200

    working but not yet streaming and just jsonld

--- quaff-lod-worker-bundle.js
+++ quaff-lod-worker-bundle.js
@@ -9241,60 +9241,46 @@ function config (name) {
 "use strict";
 
 const JsonLdParser = require("jsonld-streaming-parser").JsonLdParser;
-
+/*
+  It should be strai
+// https://github.com/rdfjs/rdfxml-streaming-parser.js
+const RdfXmlParser = require("rdfxml-streaming-parser").RdfXmlParser;
+*/
 
 let repost = function(type, dataFromParser) {
   //console.log(type, dataFromParser);
   self.postMessage({type: type, data: dataFromParser});
 }
-let parser = new JsonLdParser();
-let consumeStream = function({done, value}){
-};
-let fetchSuccess = function(response) {
-  console.log(response);
-
-  let drink = ({done, value}) => {
-    console.info("whatWhat.keys()",Object.keys(whatWhat));
-    if (done) {
-      
-    }
-    console.log("value:", value);
-                
-    parser.import(value).
-      on('context', (data) => {repost('context', data)}).
-      on('data',    (data) => {repost('data', data)}).
-      on('error',   (data) => {repost('error', data)}).
-      on('end',     (data) => {repost('end', data)});
-  };
-
-  let guzzle = function(reader) {
-    let parser = new JsonLdParser();
-    //    reader.on = console.log;
-    console.log('reader keys():', Object.keys(reader))
-    parser.import(reader.data).
-      on('context', (data) => {repost('context', data)}).
-      on('data',    (data) => {repost('data', data)}).
-      on('error',   (data) => {repost('error', data)}).
-      on('end',     (data) => {repost('end', data)});
-  }
-  /*
-  response.text().then(function(text) {
-    console.log("response.text",text);
-  });
-  */
-  //response.body.text.then((data) => console.log(data)).catch(console.log);
-  //console.log("body.text()",
-  // https://developer.mozilla.org/en-US/docs/Web/API/Body/body
-  let reader = response.body.getReader();
-  reader.read().then(guzzle).catch(console.warn);
-}
 
 self.onmessage = function(event) {
-  console.log("Worker",event);
   let url = event.data.url;
-  fetch(url).then(fetchSuccess).catch(console.log);
-  //self.postMessage('sweet jaysus: '+Object.keys(parser).join(', '));
-  //self.postMessage(event.data)
+  var parser;
+  if (url.endsWith('jsonld')) {
+    parser = new JsonLdParser();
+  } else {
+    throw new Error(`Not yet handling ${url} just .jsonld`);
+  }
+   parser
+    .on('context', (data) => {repost('context', data)})
+    .on('data',    (data) => {self.postMessage(data)})
+    .on('error',   (data) => {repost('error', data)})
+    .on('end',     (data) => {repost('end', data)});
+  fetch(url)
+    // Until streaming is solved, do the whole response at one go.
+    .then(response => response.text())
+    .then(text => {
+      parser.write(text);
+      parser.end();
+    })
+    .catch(console.log);
+  /*
+    // TODO figure out how to feed a stream to the parser
+    //   https://developer.mozilla.org/en-US/docs/Web/API/Body/body
+    // Is the problem that parser.import expects a NodeJS EventEmitter?
+    .then(response => response.body)
+    .then(body => body.getReader())
+    .then(stream => parser.import(stream))
+  */
 }
 
 
